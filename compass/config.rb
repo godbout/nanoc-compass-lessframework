3ea78b0ea530f31504b0a84566d608033d3e8b4a
Guill Lo <guill.bout@gmail.com>
2011-04-25 18:59:42 +0800

    Base for nanoc, compass and lessframework integration
    This is a structure/configuration that can be used to develop using nanoc with compass (sass/scss language) and lessframework (responsive layout) integrations.

---
+++ compass/config.rb
@@ -0,0 +1,17 @@
+# Require any additional compass plugins here.
+require 'less'
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+project_path = "."
+css_dir = "output/assets/style"
+sass_dir = "content/assets/style"
+images_dir = "output/assets/images"
+javascripts_dir = "content/assets/js"
+output_style = :expanded
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+sass_options = {
+  :syntax => :scss
+}
